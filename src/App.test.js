873b72bdb1cd770f7631866a1fdacb3a9e06c776
kaifive <tuongkhai.nguyen@gmail.com>
2021-07-20 17:17:59 -0400

    Update npm run test script

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,74 @@
 import React from 'react'
 import { shallow } from 'enzyme/build'
+import 'jest-canvas-mock';
+
 import App from './App'
-import ChartLineSimple from './views/charts/ChartLineSimple'
+
 import Dashboard from './views/dashboard/Dashboard.js'
+import Settings from './views/settings/Settings.js'
 
+import Registration from './views/registration/Registration.js'
+import CommitteeAllotments from './views/registration/CommitteeAllotments.js'
+import PaymentInvoicing from './views/registration/PaymentInvoicing.js'
+import PositionInvoicing from './views/registration/PositionInvoicing.js'
 
-it('mounts App without crashing', () => {
+import CommitteeRoster from './views/committees/CommitteeRoster.js'
+
+import CommitteeAwards from './views/awards/CommitteeAwards'
+import DelegationAwards from './views/awards/DelegationAwards'
+import ParticipationAwards from './views/awards/ParticipationAwards'
+
+it('Manuel App mounts without crashing', () => {
   const wrapper = shallow(<App/>)
   wrapper.unmount()
 })
 
-it('mounts Dashboard without crashing', () => {
+it('Manuel mounts Dashboard without crashing', () => {
   const wrapper = shallow(<Dashboard/>)
   wrapper.unmount()
 })
 
-it('mounts Charts without crashing', () => {
-  const wrapper = shallow(<ChartLineSimple/> )
+it('Manuel mounts Settings without crashing', () => {
+  const wrapper = shallow(<Settings/>)
+  wrapper.unmount()
+})
+
+it('Manuel mounts Registration without crashing', () => {
+  const wrapper = shallow(<Registration/>)
+  wrapper.unmount()
+})
+
+it('Manuel mounts Committee Allotments without crashing', () => {
+  const wrapper = shallow(<CommitteeAllotments/>)
+  wrapper.unmount()
+})
+
+it('Manuel mounts Payment Invoicing without crashing', () => {
+  const wrapper = shallow(<PaymentInvoicing/>)
+  wrapper.unmount()
+})
+
+it('Manuel mounts Position Invoicing without crashing', () => {
+  const wrapper = shallow(<PositionInvoicing/>)
+  wrapper.unmount()
+})
+
+it('Manuel mounts Committee Roster without crashing', () => {
+  const wrapper = shallow(<CommitteeRoster/>)
+  wrapper.unmount()
+})
+
+it('Manuel mounts Committee Awards without crashing', () => {
+  const wrapper = shallow(<CommitteeAwards/>)
+  wrapper.unmount()
+})
+
+it('Manuel mounts Delegation Awards without crashing', () => {
+  const wrapper = shallow(<DelegationAwards/>)
+  wrapper.unmount()
+})
+
+it('Manuel mounts Participation Awards without crashing', () => {
+  const wrapper = shallow(<ParticipationAwards/>)
   wrapper.unmount()
 })
