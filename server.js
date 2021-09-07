859d6b3f22d288728deaf6628480e324cc6316d7
kaifive <tuongkhai.nguyen@gmail.com>
2021-09-07 09:54:27 -0400

    Updated server for force https

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 import sslRedirect from 'heroku-ssl-redirect';
 
+import dotenv from 'dotenv'
+dotenv.config();
+
+import express from 'express'
+import mongoose from 'mongoose'
+import path from 'path'
+import cors from 'cors'
+
+/*
 require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require ('cors');
+*/
+
 const app = express();
 const PORT = process.env.PORT || 8080
 
