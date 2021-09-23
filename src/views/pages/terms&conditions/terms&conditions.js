dadfdc58b1059a808a2192833bb8efabe20721d8
kaifive <tuongkhai.nguyen@gmail.com>
2021-09-23 14:03:03 -0400

    Terms and conditions update

--- src/views/pages/terms&conditions/terms&conditions.js
+++ src/views/pages/terms&conditions/terms&conditions.js
@@ -46,7 +46,7 @@ const TermsAndConditions = () => {
                   <h5><strong>Cookies</strong></h5>
                   <hr></hr>
                 </p>
-                <p>We employ the use of cookies. By accessing munwell.com, you agreed to use cookies in agreement with the MUNWell, LLC's Privacy Policy.</p>
+                <p>We employ the use of cookies. By accessing <a href="/">munwell.com</a>, you agreed to use cookies in agreement with the MUNWell, LLC's Privacy Policy.</p>
                 <p>Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
                 <p>
                   <br></br>
