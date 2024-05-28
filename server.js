ca42345c52180e7c5919b768ebda43aafb6fa9bd
unknown <tuongkhai.nguyen@gmail.com>
2024-05-28 12:00:40 -0400

    Deploying to github pages

--- server.js
+++ server.js
@@ -3,15 +3,21 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 8080
+
+app.get('*', function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] != 'https')
+        res.redirect('kaifive.github.io/MUNWell' + req.url)
+    else
+        next()
+})
+
+app.set('port', (process.env.PORT || 8080))
 
 const routes = require('./src/routes/api')
 
-const MONGODB_URI = process.env.REACT_MONGO_URI
-
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.REACT_MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
@@ -21,9 +27,17 @@ mongoose.connection.on('connected', () => {
     console.log('MongoDB has been connected!!!')
 })
 
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('./build'))
+}
+
 app.use('/api', routes);
 
-app.listen(PORT, console.log(`Server is running at ${PORT}...`))
+app.listen(app.get('port'), function () {
+    console.log('Node server is running on port ' + app.get('port'));
+});
